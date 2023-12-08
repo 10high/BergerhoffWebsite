46d67b3d1c8a31d3de3f2b2ec83b727d51ef2d6b
Michael Waaler <michael@flyingtens.com>
2023-12-08 12:43:48 +0100

    fix: added error catching in fetch request and fixed ical parsing for longer descriptions

--- index.js
+++ index.js
@@ -55,136 +55,153 @@ const showStaffInfo = (event) => {
 };
 
 async function fetchHolidayData() {
-  const response = await fetch("./php/icalProxy.php");
-  const data = await response.text();
-  const elsToAppend = [];
-  const calendarArr = [];
-  const holidayData = {
-    startDate: "",
-    endDate: "",
-    vertreter: [],
-    allUpcoming: [],
-  };
-  const newDate = new Date();
-  const currentDate = Number(
-    `${newDate.getFullYear()}${newDate.getMonth() + 1}${newDate.getDate()}`
-  );
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch("./php/icalProxy.php");
+      //const reponse = new Error();
 
-  //format the data for consumption
-  const dataArr = data.split("\n");
-  dataArr.forEach((el, index) => (dataArr[index] = el.trim()));
-  const formattedData = dataArr.filter((el) => el !== "");
-
-  //add each event to array as object
-  let dataIndex = 0;
-  while (dataIndex < formattedData.length) {
-    if (formattedData[dataIndex] === "BEGIN:VEVENT") {
-      const endIndex = formattedData.indexOf("END:VEVENT", dataIndex);
-      const eventSlice = formattedData.slice(dataIndex, endIndex + 1);
-      const event = {};
-      for (const eventItem of eventSlice) {
-        const [key, value] = eventItem.split(":");
-        event[key] = value;
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-      calendarArr.push(event);
-      //move to end of event
-      dataIndex = endIndex + 1;
-      continue;
+      const data = await response.text();
+      resolve(data);
+    } catch (error) {
+      console.log(error);
+      reject(error);
     }
-    dataIndex++;
-  }
+  });
+}
 
-  //find the next holiday event, if any
-  for (const event of calendarArr) {
-    let {
-      "DTSTART;VALUE=DATE": eventStartDate,
-      "DTEND;VALUE=DATE": eventEndDate,
-      UID: uid,
-    } = event;
-    eventStartDate = Number(eventStartDate);
-    eventEndDate = Number(eventEndDate);
+async function displayHolidayData() {
+  const elsToAppend = [];
 
-    //currently on holiday?
-    if (eventStartDate < currentDate && eventEndDate > currentDate) {
-      holidayData.allUpcoming.push({ id: uid, startDate: eventStartDate });
+  try {
+    const data = await fetchHolidayData();
+
+    const calendarArr = [];
+    const holidayData = {
+      startDate: "",
+      endDate: "",
+      vertreter: [],
+      allUpcoming: [],
+    };
+    const newDate = new Date();
+    const currentDate = Number(
+      `${newDate.getFullYear()}${newDate.getMonth() + 1}${newDate.getDate()}`
+    );
+
+    //format the data for consumption
+    const allEvents = data.match(/BEGIN:VEVENT[\s\S]*?END:VEVENT/g);
+    if (allEvents) {
+      for (const event of allEvents) {
+        const startDate = event.match(/(?<=DTSTART;VALUE=DATE:).*/g)[0];
+        const endDate = event.match(/(?<=DTEND;VALUE=DATE:).*/g)[0];
+        const uid = event.match(/(?<=UID:).*/g)[0];
+        const descriptionRaw = event.match(
+          /(?<=DESCRIPTION:)[\s\S]*(?=LAST-MODIFIED:)/g
+        )[0];
+        const description = descriptionRaw.replace(/\r\s+/g, "");
+        calendarArr.push({
+          eventStartDate: Number(startDate),
+          eventEndDate: Number(endDate),
+          uid: uid,
+          description: description,
+        });
+      }
     }
-    //future holiday within 3 weeks?
-    if (eventStartDate > currentDate) {
-      const eventStartStr = `${eventStartDate}`;
-      const newEventStartDate = new Date(
-        eventStartStr.substring(0, 4),
-        parseInt(eventStartStr.substring(4, 6)) - 1,
-        eventStartStr.substring(6, 8)
-      );
-      const timeDifference = newEventStartDate.getTime() - newDate.getTime();
-      const daysDifference = timeDifference / (1000 * 3600 * 24);
-      if (daysDifference < 21) {
+
+    //find the next holiday event, if any
+    for (const event of calendarArr) {
+      const { eventStartDate, eventEndDate, uid } = event;
+
+      //currently on holiday?
+      if (eventStartDate <= currentDate && eventEndDate >= currentDate) {
         holidayData.allUpcoming.push({ id: uid, startDate: eventStartDate });
       }
+      //future holiday within 3 weeks?
+      if (eventStartDate > currentDate) {
+        const eventStartStr = `${eventStartDate}`;
+        const newEventStartDate = new Date(
+          eventStartStr.substring(0, 4),
+          parseInt(eventStartStr.substring(4, 6)) - 1,
+          eventStartStr.substring(6, 8)
+        );
+        const timeDifference = newEventStartDate.getTime() - newDate.getTime();
+        const daysDifference = timeDifference / (1000 * 3600 * 24);
+        if (daysDifference < 21) {
+          holidayData.allUpcoming.push({ id: uid, startDate: eventStartDate });
+        }
+      }
     }
-  }
+    if (holidayData.allUpcoming.length) {
+      //sort all by the earliest starting date & set that data
+      const eventsSortedByDate = holidayData.allUpcoming.sort(
+        (a, b) => a.startDate - b.startDate
+      );
 
-  if (holidayData.allUpcoming.length) {
-    //sort all by the earliest starting date & set that data
-    const eventsSortedByDate = holidayData.allUpcoming.sort(
-      (a, b) => a.startDate - b.startDate
-    );
+      const eventDataToDisplay = calendarArr.filter(
+        (obj) => obj["uid"] === eventsSortedByDate[0]["id"]
+      );
 
-    const eventDataToDisplay = calendarArr.filter(
-      (obj) => obj["UID"] === eventsSortedByDate[0]["id"]
-    );
-
-    const {
-      "DTSTART;VALUE=DATE": eventStartDate,
-      "DTEND;VALUE=DATE": eventEndDate,
-      DESCRIPTION:
+      const {
+        eventStartDate,
+        eventEndDate,
         description = "Informationen über unsere Vertretung hören Sie über die telefonische Bandansage.",
-    } = eventDataToDisplay[0];
+      } = eventDataToDisplay[0];
 
-    function formatDateInGerman(YYYYMMDD) {
-      const year = Number(YYYYMMDD.substring(0, 4));
-      const month = Number(YYYYMMDD.substring(4, 6)) - 1;
-      const day = Number(YYYYMMDD.substring(6, 8));
+      function formatDateInGerman(YYYYMMDD) {
+        const year = Number(YYYYMMDD.substring(0, 4));
+        const month = Number(YYYYMMDD.substring(4, 6)) - 1;
+        const day = Number(YYYYMMDD.substring(6, 8));
 
-      const dateObject = new Date(year, month, day);
+        const dateObject = new Date(year, month, day);
 
-      const germanDate = new Intl.DateTimeFormat("de-DE", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }).format(dateObject);
+        const germanDate = new Intl.DateTimeFormat("de-DE", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }).format(dateObject);
 
-      return germanDate;
+        return germanDate;
+      }
+
+      holidayData.startDate = formatDateInGerman(`${eventStartDate}`);
+      holidayData.endDate = formatDateInGerman(`${eventEndDate}`);
+      holidayData.vertreter = description.split("\\,");
     }
 
-    holidayData.startDate = formatDateInGerman(eventStartDate);
-    holidayData.endDate = formatDateInGerman(eventEndDate);
-    holidayData.vertreter = description.split("\\,");
-  }
+    if (holidayData.allUpcoming.length) {
+      const holidayDatesEl = document.createElement("p");
+      holidayDatesEl.innerHTML = `Wir machen Urlaub von: <br> <span class="urlaubHighlightDates">${holidayData.startDate}</span> bis <span class="urlaubHighlightDates">${holidayData.endDate}</span>.`;
+      elsToAppend.push(holidayDatesEl);
 
-  if (holidayData.allUpcoming.length) {
-    const holidayDatesEl = document.createElement("p");
-    holidayDatesEl.innerHTML = `Wir machen Urlaub von: <br> <span class="urlaubHighlightDates">${holidayData.startDate}</span> bis <span class="urlaubHighlightDates">${holidayData.endDate}</span>.`;
-    elsToAppend.push(holidayDatesEl);
+      const holidayVertreterIntroEl = document.createElement("p");
+      holidayVertreterIntroEl.classList.add("urlaubSpacer");
+      const holidayVertreterIntroText = document.createTextNode(
+        "Wir werden vertreten von:"
+      );
+      holidayVertreterIntroEl.appendChild(holidayVertreterIntroText);
+      elsToAppend.push(holidayVertreterIntroEl);
 
-    const holidayVertreterIntroEl = document.createElement("p");
-    holidayVertreterIntroEl.classList.add("urlaubSpacer");
-    const holidayVertreterIntroText = document.createTextNode(
-      "Wir werden vertreten von:"
-    );
-    holidayVertreterIntroEl.appendChild(holidayVertreterIntroText);
-    elsToAppend.push(holidayVertreterIntroEl);
-
-    const holidayVertreterULEl = document.createElement("ul");
-    for (vertreter of holidayData.vertreter) {
-      const li = document.createElement("li");
-      li.innerHTML = `<span class="urlaubHighlightNames">${vertreter}</span>`;
-      holidayVertreterULEl.appendChild(li);
+      const holidayVertreterULEl = document.createElement("ul");
+      for (vertreter of holidayData.vertreter) {
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="urlaubHighlightNames">${vertreter}</span>`;
+        holidayVertreterULEl.appendChild(li);
+      }
+      elsToAppend.push(holidayVertreterULEl);
+    } else {
+      //No holiday planned
+      const noHolidayEl = document.createElement("p");
+      noHolidayEl.innerText =
+        "Zurzeit kein Urlaub geplant. Wir sind für Sie da.";
+      elsToAppend.push(noHolidayEl);
     }
-    elsToAppend.push(holidayVertreterULEl);
-  } else {
+  } catch (error) {
+    //fetch error, display error message
     const noHolidayEl = document.createElement("p");
-    noHolidayEl.innerText = "Zurzeit kein Urlaub geplant. Wir sind für Sie da.";
+    noHolidayEl.innerText =
+      "Oops! Etwas ist schiefgelaufen. Bitte aktualisiere die Seite!";
     elsToAppend.push(noHolidayEl);
   }
   const urlaubsnotizen = document.getElementById("urlaubsnotizen");
@@ -193,4 +210,4 @@ async function fetchHolidayData() {
 
 addEvent2Array(querySelectAllArr(".heads"), "click", showStaffInfo);
 addEvent2Array(querySelectAllArr(".heads"), "keydown", showStaffInfo);
-document.addEventListener("DOMContentLoaded", fetchHolidayData);
+document.addEventListener("DOMContentLoaded", displayHolidayData);
